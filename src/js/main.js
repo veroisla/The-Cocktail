788d39f181237aa7a4a07699f7e85bc783f95317
Veronica isla <veroislagleaniz@gmail.com>
2022-04-07 18:19:52 +0200

    created functions to paint list of drinks

--- src/js/main.js
+++ src/js/main.js
@@ -1,2 +1,36 @@
 'use strict';
-console.log('holis');
+
+const listDrinks = document.querySelector('.js_listDrinks');
+const input = document.querySelector('.js_input');
+const buttonSearch = document.querySelector('.js_buttonSearch');
+const buttonReset = document.querySelector('.js_buttonReset');
+let drinks = [];
+
+//PINTAR HTML
+
+function renderListDrinks() {
+  let html = '';
+  for (const drinksOptions of drinks) {
+    html += `<li>`;
+    html += `<h3>${drinksOptions.strDrink}</h3>`;
+    html += `<img src=${drinksOptions.strDrinkThumb} alt="Imagen Bebida" class="drinks_Image"/>`;
+    html += `</li>`;
+  }
+  listDrinks.innerHTML = html;
+}
+
+function handleClickResearch() {
+  //Llamo al fecth, cuando la usuaria hace click en buscar
+  fetch(
+    `https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${input.value}`
+  )
+    .then((response) => response.json())
+    .then((data) => {
+      drinks = data.drinks;
+
+      renderListDrinks();
+    });
+}
+
+//EVENTOS BOTONES
+buttonSearch.addEventListener('click', handleClickResearch);
