47ada1adeb52906b9e3781ba12a3550821a69a19
Veronica isla <veroislagleaniz@gmail.com>
2022-04-08 09:18:00 +0200

    added function listener to each element of the list

--- src/js/main.js
+++ src/js/main.js
@@ -4,29 +4,51 @@ const listDrinks = document.querySelector('.js_listDrinks');
 const input = document.querySelector('.js_input');
 const buttonSearch = document.querySelector('.js_buttonSearch');
 const buttonReset = document.querySelector('.js_buttonReset');
-let drinks = [];
+let drinks = []; //listado completo de bebidas
+let favouriteDrinks = []; //listado bebidas favoritas
+
+// CURRENT TARGET PARA SABER A QUE ELEMENTO DAMOS CLICK
+function handleClickDrink(event) {
+  console.log(event.currentTarget.id);
+}
 
 //PINTAR HTML
 
 function renderListDrinks() {
   let html = '';
   for (const drinksOptions of drinks) {
-    html += `<li>`;
-    html += `<h3>${drinksOptions.strDrink}</h3>`;
+    html += `<li class= "js_liDrinks" id= ${drinksOptions.idDrink} >`;
+    html += `<h3> ${drinksOptions.strDrink}</h3>`;
     html += `<img src=${drinksOptions.strDrinkThumb} alt="Imagen Bebida" class="drinks_Image"/>`;
     html += `</li>`;
   }
   listDrinks.innerHTML = html;
+  //llamamos a función listenerDrinks, una vez se han pintado las paletas
+  listenerDrinks();
+}
+
+//ESCUCHAMOS EVENTO CLICK ASOCIADO A CADA COCKTAIL
+function listenerDrinks() {
+  const liDrinks = document.querySelectorAll('.js_liDrinks');
+  for (const itemDrinks of liDrinks) {
+    itemDrinks.addEventListener('click', handleClickDrink);
+  }
 }
 
 function handleClickResearch() {
-  //Llamo al fecth, cuando la usuaria hace click en buscar, es entonces cuando me sale la lista de bebidas que incluyen lo que escribe la usuaria
   fetch(
     `https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${input.value}`
   )
     .then((response) => response.json())
     .then((data) => {
       drinks = data.drinks;
+      // const image = document.createElement('img');
+      // const imageCocktail = './images/cocktail.jpg';
+      // if (strDrinkThumb === null) {
+      //   img.src = data.imageCocktail;
+      // } else {
+      //   img.src = drinksOptions.strDrinkThumb;
+      // }
 
       renderListDrinks();
     });
