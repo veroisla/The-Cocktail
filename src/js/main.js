b5202e8249927a93bfbb1b282be68e47d29af767
Veronica isla <veroislagleaniz@gmail.com>
2022-04-09 10:15:00 +0200

    started with decoration

--- src/js/main.js
+++ src/js/main.js
@@ -4,18 +4,18 @@ const listDrinks = document.querySelector('.js_listDrinks');
 const input = document.querySelector('.js_input');
 const buttonSearch = document.querySelector('.js_buttonSearch');
 const buttonReset = document.querySelector('.js_buttonReset');
-const listFavouriteDrinks = document.querySelector('.js_favouriteDrinks');
+let listFavouriteDrinks = document.querySelector('.js_favouriteDrinks');
 let drinks = []; //listado completo de bebidas
 let favouriteDrinks = []; //listado bebidas favoritas
 
-//PINTAR HTML
+//PINTAR LISTA TOTAL BEBIDAS
 
 function renderListDrinks() {
   let html = '';
   for (const drinksOptions of drinks) {
-    html += `<li class= "js_liDrinks" id= ${drinksOptions.idDrink} >`;
-    html += `<h3 class="nameDrinks"> ${drinksOptions.strDrink}</h3>`;
-    html += `<img src=${drinksOptions.strDrinkThumb} alt="Imagen Bebida" class="drinks_Image"/>`;
+    html += `<li class= "section__cocktails__item js_liDrinks" id= ${drinksOptions.idDrink} >`;
+    html += `<h3 class="section__cocktails__name"> ${drinksOptions.strDrink}</h3>`;
+    html += `<img src=${drinksOptions.strDrinkThumb} alt="Imagen Bebida" class="section__cocktails__image"/>`;
     html += `</li>`;
   }
   listDrinks.innerHTML = html;
@@ -31,14 +31,12 @@ function listenerDrinks() {
   }
 }
 
-//FAVORITOS
+//---FAVORITOS
 
 // CURRENT TARGET PARA SABER A QUE ELEMENTO DAMOS CLICK
 function handleClickDrink(event) {
-  // console.log(event.currentTarget.id);
   const idDrinks = event.currentTarget.id;
 
-  //Comprobar si la bebida está en el listado de favoritos: buscando en la lista de todas las bebidas, y comprobando si coincide un id en la lista de favorits que sea igual a un id de toda la lista de bebidas
   const foundFavoriteDrink = drinks.find((favorites) => {
     return favorites.idDrink === idDrinks;
   });
@@ -54,23 +52,24 @@ function handleClickDrink(event) {
   }
   //guardar en LS, mi lista de bebidas favoritas
   localStorage.setItem('listFavouriteDrinks', JSON.stringify(favouriteDrinks));
-  //PINTAR LISTA FAV
+  //LLAMO FUNCIÓN PINTAR LISTA FAV
   renderFavouriteDrinks();
 }
 
 //FUNCIÓN QUE PINTA LOS FAVORITOS EN LA LISTA DE FAVORITOS
+
 function renderFavouriteDrinks() {
   let htmlFav = '';
   for (const favouriteOptions of favouriteDrinks) {
-    htmlFav += `<li class= "favouriteDrinks js_liFavourite" ${favouriteOptions.idDrink}>`;
-    htmlFav += `<h3 class="favouriteName"> ${favouriteOptions.strDrink}</h3>`;
-    htmlFav += `<img src=${favouriteOptions.strDrinkThumb} alt="Imagen Bebida" class="drinks_Image_Favourite"/>`;
+    htmlFav += `<li class= "section__favourites__item js_liFavourite" ${favouriteOptions.idDrink}>`;
+    htmlFav += `<h3 class="section__favourites__name"> ${favouriteOptions.strDrink}</h3>`;
+    htmlFav += `<img src=${favouriteOptions.strDrinkThumb} alt="Imagen Bebida" class="section__favourites__image"/>`;
     htmlFav += `</li>`;
   }
   listFavouriteDrinks.innerHTML = htmlFav;
 }
 
-//LOCAL STORAGE
+//---LOCAL STORAGE
 
 const listFavouriteDrinksLS = JSON.parse(
   localStorage.getItem('listFavouriteDrinks')
@@ -96,10 +95,14 @@ function handleClickResearch(event) {
 }
 
 //FUNCIÓN RESET
+
 function handleReset() {
-  console.log('holia');
+  input.value = '';
+
+  localStorage.clear(favouriteDrinks);
 }
 
 //EVENTOS BOTONES
+
 buttonSearch.addEventListener('click', handleClickResearch);
 buttonReset.addEventListener('click', handleReset);
